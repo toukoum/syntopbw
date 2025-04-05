aeafb298184806ddc92aa6f001787a6559fc8ea6
toukoum <raphaelgiraud12@gmail.com>
2025-04-05 15:26:51 +0200

    submodule

--- mintPay/programs/mint-pay/src/states/collection.rs
+++
@@ -1,7 +0,0 @@
-use anchor_lang::prelude::*;
-
-#[account]
-
-pub struct Collection {
-    pub collection_address: Pubkey
-}
--- mintPay/programs/mint-pay/src/states/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod collection;
-pub mod template;
-
-pub use collection::*;
-pub use template::*;
